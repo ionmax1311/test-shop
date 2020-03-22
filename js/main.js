b32da9de3a4a34cf425d132c5587e910b2c15fa1
ionmax1311 <ionmax1311@gmail.com>
2020-03-22 23:48:36 +0200

    initial

---
+++ js/main.js
@@ -0,0 +1,46 @@
+/*--------slider hero------*/
+$('.hero-slider').slick({
+  dots: true,
+  infinite: true,
+  speed: 500,
+  fade: true,
+  cssEase: 'linear',
+  arrows: false,
+  autoplay: true
+});
+
+/*--------slider product------*/
+$('.product-slider').slick({
+  slidesToShow: 4,
+  slidesToScroll: 1,
+  // autoplay: true,
+  // autoplaySpeed: 2000,
+  nextArrow: '<i class="fas fa-chevron-left prev-arrow"></i>',
+  prevArrow: '<i class="fas fa-chevron-right next-arrow"></i>'
+});
+
+/*--------subscrime form validation------*/
+$('.subscribe-form').validate({
+  rules: {
+    name: {
+      required: true,
+      minlength: 1
+    },
+    company: {
+      required: false,
+      minlength: 1
+    },
+    email: {
+      required: true,
+      email: true
+    }
+  }
+});
+
+jQuery.validator.setDefaults({
+  debug: true
+});
+
+$('.subscribe-form__button').click(function() {
+  $('.subscribe-form').valid();
+});
